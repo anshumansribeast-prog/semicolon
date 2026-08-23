b8dc728027e8f858166982f262cae6b317861bcf
Anshuman Srivastava <anshumansribeast@gmail.com>
2026-08-23 10:26:20 +0530

    Fix Ada default visitor identity

--- docker/40-start-ada.sh
+++ docker/40-start-ada.sh
@@ -3,4 +3,5 @@
 # nginx starts. Ada answers /api/ada; nginx still serves the pages.
 export ADA_HOST="${ADA_HOST:-127.0.0.1}"
 export ADA_PORT="${ADA_PORT:-8420}"
+export ADA_VISITOR_NAME="${ADA_VISITOR_NAME:-}"
 python3 /opt/ada/ada_server.py &
